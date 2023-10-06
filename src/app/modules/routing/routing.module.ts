76edb17c7d97b3ab4905fc2093171cd42dfc6be6
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-06 10:32:59 -0300

    agregar productos

--- src/app/modules/routing/routing.module.ts
+++ src/app/modules/routing/routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
 import { InicioComponent } from '../inicio/components/inicio/inicio.component';
+import { ProductosComponent } from '../productos/components/productos/productos.component';
 
 const routes: Route[] = [
-  {path: '', redirectTo: '/inicio', pathMatch: 'full'},
-  {path: 'inicio', component: InicioComponent}
+  {path: '', redirectTo: '/Inicio', pathMatch: 'full'},
+  {path: 'Inicio', component: InicioComponent},
+  {path: 'Productos', component: ProductosComponent}
 ]
 
 @NgModule({
