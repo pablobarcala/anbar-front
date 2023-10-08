7b677baa84e89a7f9aa621d2dd7347819799a488
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-08 16:38:34 -0300

    agregar categorias de admin

--- src/app/modules/admin/components/admin-categorias/admin-categorias.component.ts
+++ src/app/modules/admin/components/admin-categorias/admin-categorias.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Categoria {
+  nombre: string
+}
+
 @Component({
   selector: 'app-admin-categorias',
   templateUrl: './admin-categorias.component.html',
   styleUrls: ['./admin-categorias.component.css']
 })
 export class AdminCategoriasComponent {
-
+  categorias: Categoria[] = [
+    {
+      nombre: "Muebles"
+    }, 
+    {
+      nombre: "Almohadones"
+    }
+  ]
 }
