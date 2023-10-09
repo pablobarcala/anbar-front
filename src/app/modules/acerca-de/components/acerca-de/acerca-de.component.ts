b862b71c13b5387d58931ca511722c9a4f7b52d6
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-09 00:37:02 -0300

    agregar nosotros al acerca de

--- src/app/modules/acerca-de/components/acerca-de/acerca-de.component.ts
+++ src/app/modules/acerca-de/components/acerca-de/acerca-de.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Nosotros } from 'src/app/interfaces/Nosotros';
+import { NosotrosService } from 'src/app/services/nosotros.service';
 
 @Component({
   selector: 'app-acerca-de',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AcercaDeComponent {
 
+  nosotros: Nosotros | undefined = undefined
+
+  constructor(private nosotrosService: NosotrosService){
+    nosotrosService.getNosotros().subscribe((nosotros: any) => {
+      nosotros.find((nosotros: any) => {
+        if(nosotros.idnosotros == 1){
+          this.nosotros = nosotros
+        }
+      })
+    })
+  }
 }
