aa0f6b252fc11958fcead2fe0f4b95d091d57c15
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-09 12:28:27 -0300

    agregar funcion de editar preguntas

--- src/app/modules/admin/components/edit-faq/edit-faq.component.ts
+++ src/app/modules/admin/components/edit-faq/edit-faq.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pregunta } from 'src/app/interfaces/Pregunta';
+import { PreguntasService } from 'src/app/services/preguntas.service';
 
 @Component({
   selector: 'app-edit-faq',
@@ -6,5 +10,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-faq.component.css']
 })
 export class EditFaqComponent {
+  form: FormGroup;
+  id: number = 0
+  pregunta: Pregunta | undefined = undefined
 
+  constructor(
+    private route: ActivatedRoute,
+    private preguntasService: PreguntasService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ){    
+    route.params.subscribe(params => {
+      this.id = params['id'];
+      this.filtrar(this.id)
+    })
+
+    this.form = formBuilder.group({
+      idpreguntas: [this.id, Validators.required],
+      pregunta: ['', Validators.required],
+      respuesta: ['', Validators.required]
+    })
+  }
+
+  filtrar(id: number){
+    this.preguntasService.getPreguntas().subscribe((resp: any) => {
+      let preguntas: Pregunta[] = resp
+
+      this.pregunta = preguntas.find(pregunta => pregunta.idpreguntas == id)
+      this.actualizarForm()
+    })
+  }
+
+  actualizarForm(){
+    if(this.pregunta){
+      this.form.patchValue({
+        pregunta: this.pregunta.pregunta,
+        respuesta: this.pregunta.respuesta
+      })
+    }
+  }
+
+  editPregunta(event: Event) {
+    event.preventDefault()
+
+    if(this.form.valid){
+      this.preguntasService.editPregunta(this.form.get('idpreguntas')?.value, this.form.value).subscribe(resp => {
+        if(resp){
+          alert("Se editó correctamente")
+          this.router.navigate(['/admin/dashboard/admin-faq'])
+          .then(() => window.location.reload());
+        } else {
+          alert("Hubo un error")
+        }
+      })
+    } else {
+      this.form.markAllAsTouched()
+    }
+  }
 }
