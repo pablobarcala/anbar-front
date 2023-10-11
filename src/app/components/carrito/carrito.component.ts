cd076abf3e60582d4ed22d16a66c7bcd0cb85582
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-10 23:13:13 -0300

    primeros diseños del carrito

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/interfaces/Producto';
+import { CarritoService } from 'src/app/services/carrito.service';
 
 @Component({
   selector: 'app-carrito',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./carrito.component.css']
 })
 export class CarritoComponent {
+  productos: Producto[] = []
 
+  constructor(
+    private carritoService: CarritoService
+  ){
+    this.carritoService.getProductos().subscribe((productos: any) => this.productos = productos)
+  }
 }
