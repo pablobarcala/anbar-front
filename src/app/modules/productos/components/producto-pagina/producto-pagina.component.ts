e37a46cf906891569747b99be29b79a4cc67ae5d
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-16 20:13:44 -0300

    agregar funcion para carrito en la pagina de producto

--- src/app/modules/productos/components/producto-pagina/producto-pagina.component.ts
+++ src/app/modules/productos/components/producto-pagina/producto-pagina.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Producto } from 'src/app/interfaces/Producto';
+import { CarritoService } from 'src/app/services/carrito.service';
 import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
@@ -11,14 +13,20 @@ import { ProductosService } from 'src/app/services/productos.service';
 export class ProductoPaginaComponent {
   id: number = 0;
   producto: Producto | undefined = undefined 
+  cantidad: FormGroup
 
   constructor(
     private route: ActivatedRoute,
-    private productoService: ProductosService  
+    private productoService: ProductosService,
+    private formBuilder: FormBuilder,
+    private carritoService: CarritoService
   ){
     route.params.subscribe(params => {
       this.id = params['id']
       this.tomarProducto(this.id)
+    })
+    this.cantidad = formBuilder.group({
+      cantidad: [1]
     })
   }
 
@@ -28,5 +36,9 @@ export class ProductoPaginaComponent {
 
       this.producto = productosFiltrar.find((p: any) => p.idproductos == id)
     })
+  }
+
+  agregarCarrito(producto: Producto){
+    this.carritoService.agregarCarrito(this.cantidad.value.cantidad, producto)
   }
 }
