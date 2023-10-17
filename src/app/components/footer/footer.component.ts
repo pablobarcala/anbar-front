72a13178a76fe0d20c58fdbb57373d2eee16eb5c
Pablo Barcala <pablobarcala4@gmail.com>
2023-10-17 11:46:51 -0300

    agregar link de instagram en footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Nosotros } from 'src/app/interfaces/Nosotros';
+import { NosotrosService } from 'src/app/services/nosotros.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  nosotros: Nosotros | undefined = undefined;
 
+  constructor(
+    private nosotroService: NosotrosService
+  ) {
+    nosotroService.getNosotros().subscribe((resp: any) => {
+      let nosotrosLista: Nosotros[] = resp
+
+      nosotrosLista.find((n: any) => {
+        if(n.idnosotros == 1) {
+          this.nosotros = n
+        }
+      })
+    })
+  }
 }
